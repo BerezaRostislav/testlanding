d466624d4dc1f1f1df26ac0390d432007fc7edb9
BerezaRostislav <bereza.rostislav93@gmail.com>
2018-08-12 15:46:25 +0300

    second component fix

--- components/secondblock.vue
+++ components/secondblock.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container grid-list-xl style="padding: 0; padding-top: 12px;">
     <v-layout row wrap>
-      <v-flex xs-col-md-6 style="padding: 0">
+      <v-flex xs-col-md-6 style="padding: 0; max-width:50vw;">
           <img width="100%" src="../static/left.png">
       </v-flex>
-      <v-flex xs-col-md-6 style="padding: 0;">
+      <v-flex xs-col-md-6 style="padding: 0; max-width:50vw;">
           <img width="100%" src="../static/right.png">
       </v-flex>
     </v-layout>
@@ -13,8 +13,19 @@
 
 <style scoped>
 
+.layout {
+  max-width:100vw;
+  margin: 0px;
+}
+
+.container.grid-list-xl .layout:only-child {
+    margin-top: -12px;
+}
+
 .container {
-    margin: 0;
-    max-width: 100%;
+  padding: 0px;
+  padding-top: 12px;
+  margin-right: -12px;
+  max-width: 100%;
 }
 </style>
